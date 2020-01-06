8446b483e88ed300c97f2ce46e18dadcda3f903f
16Light <58923216+16Light@users.noreply.github.com>
2020-01-05 21:19:08 -0800

    public int movement

--- BoardGame/Assets/Camel.cs
+++ BoardGame/Assets/Camel.cs
@@ -7,7 +7,11 @@ using UnityEngine;
 public class Camel : MonoBehaviour
 {
     // creating a random number... will move to dice class
-    int movement = new System.Random().Next(1, 3);
+    // int movement = new System.Random().Next(1, 3);
+
+    public int movement;
+
+    }
 
     // Start is called before the first frame update
     void Start()

8bd40d514795b993cef8da438317e430a3b20e93
16Light <58923216+16Light@users.noreply.github.com>
2020-01-05 20:57:53 -0800

    camel color and dice generator

---
+++ BoardGame/Assets/Camel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Camel : MonoBehaviour
+{
+    // creating a random number... will move to dice class
+    int movement = new System.Random().Next(1, 3);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

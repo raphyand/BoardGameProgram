8b3ba931c0b78e3f895d8c0dce0a8c41c7b17034
16Light <58923216+16Light@users.noreply.github.com>
2020-01-07 23:11:46 -0800

    added dice colors

---
+++ BoardGame/Assets/dice.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dice : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // creating a random number... will move to dice class
+
+    int movement = new System.Random().Next(1, 3);
+     
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- BoardGame/Assets/diice_holder.cs
+++ BoardGame/Assets/diice_holder.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class diice_holder : MonoBehaviour
 
-public class Camel : MonoBehaviour
+    
+    disp = false 
+
 {
-
-    public int movement;
-
     // Start is called before the first frame update
     void Start()
     {

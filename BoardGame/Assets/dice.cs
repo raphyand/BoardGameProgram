fc5a515ecd217c245ba9b608a7428c5f23e65826
raphyand <raphyand@gmail.com>
2020-01-22 21:52:14 -0800

    Revamp attempt at Dice

--- BoardGame/Assets/dice.cs
+++ BoardGame/Assets/dice.cs
@@ -5,24 +5,24 @@ using UnityEngine;
 public class dice : MonoBehaviour
 {
     // Start is called before the first frame update
-
     // creating a random number... will move to dice class
 
-     
+    [SerializeField] int movementValue;
+     public bool positive;
+     [SerializeField] 
+
+     public void generateMovementValue()
+     {
+          if (positive == true)
+               movementValue = new System.Random().Next(1, 3);
+          if (positive == false)
+               movementValue = new System.Random().Next(1, 3) * -1;
+     }
+
+     void Start()
+     {
+          generateMovementValue();
+     }
 
 
-
-
-    int movement = new System.Random().Next(1, 3);
-     
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- BoardGame/Assets/diice_holder.cs
+++ BoardGame/Assets/diice_holder.cs
@@ -5,45 +5,31 @@ using UnityEngine;
 public class diice_holder : MonoBehaviour
 {
      // Need to have 5 out of 7 objects get spit out.
-     public GameObject[] diceHolder;
-     enum DiceColors { BLUE, ORANGE, YELLOW, RED, GREEN, BLACK, WHITE};
-
-
-
-     
-
-
-     int randomGenerator() // Randomly take out a die
-     {
-          int diceSelected = new System.Random().Next(0, 6);
-          return diceSelected;
-     }
-
-     GameObject throwDice()
-     {
-          int numberThrown = randomGenerator();
-          return diceHolder[numberThrown];
-     }
-
-
+     public dice[] diceHolder;
 
      // Start is called before the first frame update
      void Start()
     {
-          for (int i = 0; i < 5; i++)
-          {
-               GameObject myObject = throwDice();
-               string objName = myObject.name;
-               Debug.Log("The name of this element is " + objName);
-          }
-
+          rollDice();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+     void rollDice()
+     {
+          
+          for (int i = 0; i < 5; i++)
+          {
+               diceHolder[new System.Random().Next(1, 3)].generateMovementValue();
+
+          }
+          
+
+     }
 
 
 }

9f6a5650a6f45255d8f8807f6bab93bf0236f478
raphyand <raphyand@gmail.com>
2020-01-22 22:07:03 -0800

    Seemingly Working RNG

--- BoardGame/Assets/dice.cs
+++ BoardGame/Assets/dice.cs
@@ -7,16 +7,16 @@ public class dice : MonoBehaviour
     // Start is called before the first frame update
     // creating a random number... will move to dice class
 
-    [SerializeField] int movementValue;
+    [SerializeField] float movementValue;
      public bool positive;
      [SerializeField] 
 
      public void generateMovementValue()
      {
           if (positive == true)
-               movementValue = new System.Random().Next(1, 3);
+               movementValue = Random.Range(0, 3);
           if (positive == false)
-               movementValue = new System.Random().Next(1, 3) * -1;
+               movementValue = Random.Range(0, 3) * -1;
      }
 
      void Start()
